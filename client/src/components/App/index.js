c24e806ed9172de19e9196c6d11759a78087ce26
Aurelie Chicart <aurelie.chicart@gmail.com>
2021-09-25 16:19:52 +0200

    setting up express server and mock route, setting up an automatic proxy and testing API request on client side

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,29 @@
+import axios from 'axios';
+
 import './app.scss';
 
 import Navbar from '../Navbar';
 import Page from '../../containers/Page';
+import { useEffect } from 'react';
 
 function App() {
+
+  // testing API request through proxy
+  const loadAPIText = () => {
+    axios.get('/api')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  useEffect(() => {
+    loadAPIText();
+  }, []);
+
+
   return (
     <div className="App">
       <Navbar />
---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Helmet helps protect the app from some well-known web vulnerabilities by setting HTTP headers appropriately
+const helmet = require('helmet');
+
+app.use(helmet());
+
+// const router = require('./app/router');
+
+// Middleware which parses incoming requests with JSON payloads
+app.use(express.json());
+
+// app.use('/api/', router);
+app.get('/api', (req, res) => {
+  res.send("hello");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on : localhost:${PORT}/api/`)
+});
