3f0c12eb7b598e89f9e1e132d95805dca4fc1e20
Aurelie Chicart <aurelie.chicart@gmail.com>
2021-09-30 19:43:58 +0200

    adding controller methods for retrieving campgrounds

--- server/app/controllers/campgroundController.js
+++ server/app/controllers/campgroundController.js
@@ -1,16 +1,36 @@
-// const Campground = require ('../models/campground');
-const db = require('../database');
+const Campground = require('../models/campground');
 
 const campgroundController = {
-
+  /**
+      * Controls endpoint GET /api/campgrounds
+      */
   getAll: async (req, res) => {
     try {
-      const { rows } = await db.query('SELECT * FROM campground;');
-      res.json(rows);
+      const theCampgrounds = await Campground.findAll();
+      res.status(200).json(theCampgrounds);
 
     } catch (err) {
       res.status(404).json(err.message);
     }
+  },
+
+   /**
+    * Controls endpoint GET /api/campgrounds/:id
+    */
+    getOneCampground: async (req, res) => {
+
+      try {
+          // We get the id in the parameters of the request
+          const { id } = req.params;
+
+          const oneCampground = await Campground.findOne(id);
+
+          res.status(200).json(oneCampground);
+
+
+      } catch (err) {
+          res.status(404).json(err.message);
+      };
   },
 
 };
--- server/app/router.js
+++ server/app/router.js
@@ -1,8 +1,24 @@
 const { Router } = require('express');
 const router = Router();
 
+// Controllers
 const campgroundController = require('./controllers/campgroundController');
 
+/**
+ * Returns all campgrounds from the database
+ * @route GET /campgrounds
+ * @group Campgrounds
+ * @returns {Array<Campground>} 200 - An array of campgrounds
+ */
 router.get('/campgrounds', campgroundController.getAll);
+
+/**
+ * Returns a campground from the database based on its id
+ * @route GET /campgrounds/{id}
+ * @group Campgrounds
+ * @param {number} id.path.required - the campground id
+ * @returns {<Campground>} 200 - An instance of a campground
+ */
+router.get('/campgrounds/:id', campgroundController.getOneCampground);
 
 module.exports = router;
